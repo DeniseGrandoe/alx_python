89354f5c3871307e5120bf47e621ccbe11516f32
DeniseGrandoe <grandoedenise@gmail.com>
2023-08-07 17:20:00 +0000

    updated

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for num in range(0, 10):
-    for n in range(0, 10):
-        if num < n:
-            print("{:d}{:d}".format(num, n), end="")
-            if num < 8:
-                print(", ", end="")
+for num in range(10):
+    for n in range(10):
+        if num == 8 and n == 9:
+            print("89")
+        elif num < n:
+            print("{:d}{:d}".format(num, n), end=", ")
             
